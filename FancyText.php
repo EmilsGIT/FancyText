08cb1939eb8c03220aa5f1b4570efff7cbdcd3e1
EmilsGIT <email.brink96@gmail.com>
2019-11-18 19:34:16 +0100

    Works but has minor issues

--- FancyText.php
+++ FancyText.php
@@ -12,4 +12,21 @@ echo "What do you think you're doing?";
 exit;
 }
 
+class FancyText{
+    function __construct(){
+        add_action("init", array($this, "RunFilters"));
+    }
 
+    function FancyWordThing($and){
+        $and = str_replace ("and", "further more", $and);
+        return $and;
+    }
+
+    function RunFilters(){
+        add_filter("the_content", array($this, "FancyWordThing"));
+    }
+}
+
+if (class_exists('FancyText')) {
+    $makeChangesToText = new FancyText();
+}
