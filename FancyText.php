c40ebf7fb197426534a8e51fcee08e4cdf48ce01
EmilsGIT <email.brink96@gmail.com>
2019-11-18 18:53:40 +0100

    text

---
+++ FancyText.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name:  FancyText
+Description:  Plugin that makes text fancy
+Version:      1.0.0
+Author:       Emil
+Text Domain:  Da Boi
+ */
+
+if(! function_exists("add_action")){
+echo "What do you think you're doing?";
+exit;
+}
+
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+//No.
+
+if(! function_exists("add_action")){
+    echo "What do you think you're doing?";
+    exit;
+    }
+    
